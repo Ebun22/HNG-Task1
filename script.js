48155f4d19abab2073caebfe2f06660bfe465444
Ebun22 <subeydavid22@gmail.com>
2023-09-07 12:33:38 +0100

    added javascript

---
+++ script.js
@@ -0,0 +1,27 @@
+console.log("connected")
+
+function currentTime() {
+    const daysOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
+    const currentTime = new Date();
+    const day = currentTime.getDay()
+    const hour = currentTime.getHours();
+    const minute = currentTime.getMinutes() ;
+    const HTMLDayOfTheWeek = document.getElementById('day')
+    const HTMLTime = document.getElementById('time')
+    // const HTMLMeridiem = document.getElementByClassName('meridiem')
+    console.log(HTMLDayOfTheWeek )
+    let meridiem
+    if(hour >= 12){
+        meridiem = "PM"
+    }else{
+        meridiem = "AM"
+    }
+
+    const time = `${hour}:${minute} ${meridiem}`
+    HTMLTime.textContent = time;
+    HTMLDayOfTheWeek.textContent = daysOfTheWeek[day]
+   
+}
+
+currentTime();
+setInterval(currentTime(), 1000)
